2de9065c623a368fef7dd447bde995f2fbe40ea9
Moh abbh <musst92@gmail.com>
2025-04-27 05:19:07 +0300

    new analytic

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,7 @@ import Script from "next/script";
 import { Analytics } from "@vercel/analytics/react"
 import "./globals.css";
 
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -38,6 +39,7 @@ export default function RootLayout({ children }) {
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
         <Analytics />
+        
       </body>
     </html>
   );
